55d58de69f08f5dbf2947dace745cb520630e7b2
Thomas Laegler <thomas.laegler@googlemail.com>
2017-08-14 10:30:26 +1200

    Initial commit

---
+++ fabric8-example/src/main/java/io/laegler/fabric8/example/Fabric8ExampleApplication.java
@@ -0,0 +1,12 @@
+package io.laegler.fabric8.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fabric8ExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fabric8ExampleApplication.class, args);
+	}
+}
